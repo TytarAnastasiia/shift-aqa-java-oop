aaed0498d97bf4f0eff8c485fa9384f85978bc28
Anastasiia Tytar <sandyemma090403@gmail.com>
2026-06-19 15:41:25 +0700

    внесены изменения в соответствии с замечаниями

--- src/main/java/Decepticon.java
+++ src/main/java/Decepticon.java
@@ -8,6 +8,26 @@ public class Decepticon extends Transformer {
         this.teamName = teamName;
     }
 
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public boolean getKindness() {
+        return kindness;
+    }
+
+    public void setKindness(String kindness) {
+        this.kindness = Boolean.parseBoolean(kindness);
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
     public void transform() {
         System.out.println(super.name + " is transforming into a fighter jet");
     }
--- src/main/java/Transformer.java
+++ src/main/java/Transformer.java
@@ -1,24 +1,34 @@
 public class Transformer {
     // переменная класса (поле) - имя
-    protected String name;
+    private String name;
 
     // конструктор класса
     public Transformer(String name) {
         this.name = name;
     }
 
+    //метод для получения значения
+    public String getName() {
+        return name;
+    }
+
+    // метод для установки значения
+    public void setName(String name) {
+        this.name = name;
+    }
+
     // метод run (бежать)
     public void run() {
-        System.out.println(name + " is running");
+        System.out.println(this.name + " is running");
     }
 
     // метод fire (стрелять)
     public void fire() {
-        System.out.println(name + " is firing");
+        System.out.println(this.name + " is firing");
     }
 
     // метод charge (зарядиться)
     public void charge() {
-        System.out.println(name + " is charging");
+        System.out.println(this.name + " is charging");
     }
 }
